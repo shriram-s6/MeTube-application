a8345a91dc0c110e7f2bbba6e06d5e07528e3092
shriram-s6 <shriram.sekar94@gmail.com>
2022-04-11 10:36:35 -0400

    config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ ob_start();
 session_start();
 date_default_timezone_set("America/New_York");
 try {
-    $connect = new PDO("mysql:dbname=metube;host=localhost:3307", "root", "");
+    $connect = new PDO("mysql:dbname=metube;host=localhost", "root", "");
     $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 } catch (PDOException $exception) {
     echo "Connection failed: ". $exception->getMessage();
