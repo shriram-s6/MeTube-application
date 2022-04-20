4a40e4a0218b25c0efec786a2b722ad5fec44ccf
shriram-s6 <shriram.sekar94@gmail.com>
2022-04-19 20:58:05 -0400

    word cloud

--- sideNavBar.php
+++ sideNavBar.php
@@ -1,3 +1,3 @@
 <div id="sideNavContainer" style="display: none;">
-
+    <a href="wordCloud.php">Word Cloud</a>
 </div>
---
+++ wordCloud.php
@@ -0,0 +1,26 @@
+<?php require_once("header.php") ?>
+
+<html lang="en-us">
+    <head>
+        <title>MeTube</title>
+        <link rel="stylesheet" type="text/css" href="wordcloud/wordCloud.css">
+    </head>
+    <body>
+        <?php
+            $content = file_get_contents("wordcloud/words.txt");
+
+            $words = preg_split('/([a-zA-Z]+[\s]*[a-zA-Z]*)/', $content, -1, PREG_SPLIT_DELIM_CAPTURE);
+
+            echo "<p>";
+            for($i = 1; $i < count($words); $i = $i + 2) {
+                $word_count = $words[$i + 1];
+                $word_count = trim($word_count);
+                echo '<span class="'."a".$word_count.'">';
+                echo trim($words[$i]);
+                echo "  ";
+                echo "</span>";
+            }
+            echo "</p>";
+        ?>
+    </body>
+</html>
