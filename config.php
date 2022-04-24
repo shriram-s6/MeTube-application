555586cb4e11842c9f2cd9bfc08ae531cfc4465e
mbcalli <mbcalli@clemson.edu>
2022-04-24 18:50:21 -0400

    updated config

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ session_start();
 date_default_timezone_set("America/New_York");
 try {
     global $connect;
-    $connect = new PDO("mysql:dbname=metube;host=localhost", "root", "");
+    $connect = new PDO("mysql:dbname=metube-g2_p6fa;host=mysql1.cs.clemson.edu", "metube-g2_aala", "callicottsekar1");
     $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     // echo $connect;
 } catch (PDOException $exception) {
