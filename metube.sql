386efe949b0efaf76c62cfeb9b61c6de18505508
shriram-s6 <shriram.sekar94@gmail.com>
2022-04-24 18:20:00 -0400

    metube sql

---
+++ metube.sql
@@ -0,0 +1,359 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3307
+-- Generation Time: Apr 25, 2022 at 12:17 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `metube`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat`
+--
+
+DROP TABLE IF EXISTS `chat`;
+CREATE TABLE `chat` (
+  `chatId` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `toUserName` varchar(50) NOT NULL,
+  `fromUserName` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contacts`
+--
+
+DROP TABLE IF EXISTS `contacts`;
+CREATE TABLE `contacts` (
+  `id` int(11) NOT NULL,
+  `userName` varchar(25) NOT NULL,
+  `contactUserName` varchar(25) NOT NULL,
+  `blocked` tinyint(4) NOT NULL,
+  `type` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `discussion_categories`
+--
+
+DROP TABLE IF EXISTS `discussion_categories`;
+CREATE TABLE `discussion_categories` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `discussion_topics`
+--
+
+DROP TABLE IF EXISTS `discussion_topics`;
+CREATE TABLE `discussion_topics` (
+  `topic_id` int(11) NOT NULL,
+  `title` varchar(150) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `posted_by` varchar(30) NOT NULL,
+  `posted_time` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dislikes`
+--
+
+DROP TABLE IF EXISTS `dislikes`;
+CREATE TABLE `dislikes` (
+  `id` int(11) NOT NULL,
+  `userName` varchar(50) NOT NULL,
+  `commentId` int(11) NOT NULL,
+  `videoId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `file_categories`
+--
+
+DROP TABLE IF EXISTS `file_categories`;
+CREATE TABLE `file_categories` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `file_uploads`
+--
+
+DROP TABLE IF EXISTS `file_uploads`;
+CREATE TABLE `file_uploads` (
+  `id` int(11) NOT NULL,
+  `uploadedBy` varchar(50) NOT NULL,
+  `title` varchar(80) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `fileType` int(11) NOT NULL,
+  `privacy` int(11) NOT NULL DEFAULT 0,
+  `filePath` varchar(250) NOT NULL,
+  `category` int(11) NOT NULL DEFAULT 0,
+  `uploadDate` datetime NOT NULL DEFAULT current_timestamp(),
+  `views` int(11) NOT NULL DEFAULT 0,
+  `duration` varchar(10) NOT NULL,
+  `fileSize` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likes`
+--
+
+DROP TABLE IF EXISTS `likes`;
+CREATE TABLE `likes` (
+  `id` int(11) NOT NULL,
+  `userName` varchar(50) NOT NULL,
+  `commentId` int(11) NOT NULL,
+  `videoId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscribers`
+--
+
+DROP TABLE IF EXISTS `subscribers`;
+CREATE TABLE `subscribers` (
+  `subscriberId` int(11) NOT NULL,
+  `subscribedTo` varchar(50) NOT NULL,
+  `subscribedFrom` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `firstName` varchar(25) NOT NULL,
+  `lastName` varchar(25) NOT NULL,
+  `userName` varchar(25) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `signUpDate` datetime NOT NULL DEFAULT current_timestamp(),
+  `profilePic` varchar(250) NOT NULL,
+  `profileImage` longblob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_comments`
+--
+
+DROP TABLE IF EXISTS `user_comments`;
+CREATE TABLE `user_comments` (
+  `commentId` int(11) NOT NULL,
+  `commentedBy` varchar(50) NOT NULL,
+  `videoId` int(11) NOT NULL,
+  `respondedTo` int(11) NOT NULL,
+  `comment` text NOT NULL,
+  `commentedOn` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `video_thumbnails`
+--
+
+DROP TABLE IF EXISTS `video_thumbnails`;
+CREATE TABLE `video_thumbnails` (
+  `thumbnailId` int(11) NOT NULL,
+  `videoId` int(11) NOT NULL,
+  `filePath` varchar(250) NOT NULL,
+  `pickedThumbnail` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`chatId`);
+
+--
+-- Indexes for table `contacts`
+--
+ALTER TABLE `contacts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `discussion_categories`
+--
+ALTER TABLE `discussion_categories`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `discussion_topics`
+--
+ALTER TABLE `discussion_topics`
+  ADD PRIMARY KEY (`topic_id`);
+
+--
+-- Indexes for table `dislikes`
+--
+ALTER TABLE `dislikes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `file_categories`
+--
+ALTER TABLE `file_categories`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `file_uploads`
+--
+ALTER TABLE `file_uploads`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subscribers`
+--
+ALTER TABLE `subscribers`
+  ADD PRIMARY KEY (`subscriberId`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_comments`
+--
+ALTER TABLE `user_comments`
+  ADD PRIMARY KEY (`commentId`);
+
+--
+-- Indexes for table `video_thumbnails`
+--
+ALTER TABLE `video_thumbnails`
+  ADD PRIMARY KEY (`thumbnailId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chat`
+--
+ALTER TABLE `chat`
+  MODIFY `chatId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `contacts`
+--
+ALTER TABLE `contacts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `discussion_categories`
+--
+ALTER TABLE `discussion_categories`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `discussion_topics`
+--
+ALTER TABLE `discussion_topics`
+  MODIFY `topic_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `dislikes`
+--
+ALTER TABLE `dislikes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `file_categories`
+--
+ALTER TABLE `file_categories`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `file_uploads`
+--
+ALTER TABLE `file_uploads`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `subscribers`
+--
+ALTER TABLE `subscribers`
+  MODIFY `subscriberId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_comments`
+--
+ALTER TABLE `user_comments`
+  MODIFY `commentId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `video_thumbnails`
+--
+ALTER TABLE `video_thumbnails`
+  MODIFY `thumbnailId` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
